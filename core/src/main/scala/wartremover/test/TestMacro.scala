f622735100817186884c3a3fc6a154d436e2990c
Ismael Juma <juma.juma@barclays.com>
2015-01-19 17:35:49 +0000

    Take multiple `WartTraverser` values in `WartTestTraverser.apply`.
    Co-authored by @milessabin.

--- core/src/main/scala/wartremover/test/TestMacro.scala
+++ core/src/main/scala/wartremover/test/TestMacro.scala
@@ -7,22 +7,27 @@ import reflect.macros.Context
 object WartTestTraverser {
   case class Result(errors: List[String], warnings: List[String])
 
-  def apply(t: WartTraverser)(a: Any): Result = macro applyImpl
-  def applyImpl(c: Context)(t: c.Expr[WartTraverser])(a: c.Expr[Any]) = {
+  def apply(t: WartTraverser*)(a: Any): Result = macro applyImpl
+  def applyImpl(c: Context)(t: c.Expr[WartTraverser]*)(a: c.Expr[Any]) = {
     import c.universe._
-
-    val traverser = c.eval[WartTraverser](c.Expr(c.resetLocalAttrs(t.tree.duplicate)))
 
     var errors = collection.mutable.ListBuffer[String]()
     var warnings = collection.mutable.ListBuffer[String]()
 
-    object MacroTestUniverse extends WartUniverse {
-      val universe: c.universe.type = c.universe
-      def error(pos: universe.Position, message: String) = errors += message
-      def warning(pos: universe.Position, message: String) = warnings += message
-    }
+    t.foreach { expr =>
 
-    traverser(MacroTestUniverse).traverse(a.tree)
+      val traverser = c.eval[WartTraverser](c.Expr(c.resetLocalAttrs(expr.tree.duplicate)))
+
+      object MacroTestUniverse extends WartUniverse {
+        val universe: c.universe.type = c.universe
+
+        def error(pos: universe.Position, message: String) = errors += message
+
+        def warning(pos: universe.Position, message: String) = warnings += message
+      }
+
+      traverser(MacroTestUniverse).traverse(a.tree)
+    }
 
     c.Expr(q"WartTestTraverser.Result(List(..${errors.toList}), List(..${warnings.toList}))")
   }
